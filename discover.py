b2c5066e5ce0a0fe3228a37bc34e52172f59971c
Grzegorz Mrukwa <g.mrukwa@gmail.com>
2018-03-03 12:42:11 +0100

    Add backend discovery
    Squashed commits:
    
    [f931c93] Fix exception in algorithms endpoint
    
    Set is not serializable.
    
    [ab3bfe4] Convert KeyError to ValueError in discover
    
    [2826ebd] Reorganize imports in discover
    
    [9a3e06c] Fix backend discovery test
    
    [ce38a03] Add backend discovery

--- discover.py
+++ discover.py
@@ -2,6 +2,7 @@ from itertools import chain, groupby
 from operator import attrgetter
 from typing import Dict, NamedTuple, List
 
+import requests
 import celery.task.control
 
 WorkerName = str
@@ -33,28 +34,68 @@ def workers() -> Dict[WorkerName, List[Task]]:
     }
 
 
-def tasks() -> Dict[TaskType, List[TaskName]]:
+def tasks(all_workers: Dict[WorkerName, List[Task]]=None) \
+        -> Dict[TaskType, List[TaskName]]:
     """Find tasks available in the cluster
 
     Returns:
         Dictionary of task types with their algorithm.
     """
-    all_tasks = list(chain.from_iterable(workers().values()))
+    if all_workers is None:
+        all_workers = workers()
+    all_tasks = list(chain.from_iterable(all_workers.values()))
     all_tasks = sorted(all_tasks, key=attrgetter('type'))
     return {
-        task_type: {task.name for task in unitype_tasks}
+        task_type: list({task.name for task in unitype_tasks})
         for task_type, unitype_tasks in groupby(all_tasks, key=attrgetter('type'))
     }
 
 
-def role(task_name: TaskName) -> TaskType:
+def role(task_name: TaskName,
+         all_tasks: Dict[TaskType, List[TaskName]]=None) -> TaskType:
     """Find type of the task.
 
     Returns:
         Type of the task.
     """
-    all_tasks = tasks()
+    if all_tasks is None:
+        all_tasks = tasks()
     for task_type in all_tasks:
         if task_name in all_tasks[task_type]:
             return task_type
-    raise KeyError(task_name)
+    raise ValueError(task_name)
+
+
+def backend(task_name: TaskName,
+            all_workers: Dict[WorkerName, List[Task]]=None) -> WorkerName:
+    """Find any worker on which the task is supported.
+
+    Returns:
+        Name of the worker that supports execution of particular task.
+    """
+    if all_workers is None:
+        all_workers = workers()
+    for worker in all_workers:
+        for some_task in all_workers[worker]:
+            if some_task.name == task_name:
+                return worker
+    raise ValueError(task_name)
+
+
+def _analyses_done(backend_name: WorkerName, task: TaskName):
+    return requests.get(backend_name + '/results/' + task).json()
+
+
+FinishedTask = Dict[str, object]
+
+
+def finished_analyses(all_workers: Dict[WorkerName, List[Task]]=None) \
+        -> Dict[TaskType, List[FinishedTask]]:
+    """Find all the finished analyses for each task."""
+    if all_workers is None:
+        all_workers = workers()
+    all_tasks = set(chain.from_iterable(tasks(all_workers).values()))
+    backends = [backend(task, all_workers) for task in all_tasks]
+    analyses = map(_analyses_done, backends, all_tasks)
+    listed = {task: finished for task, finished in zip(all_tasks, analyses)}
+    return listed
--- test/test_discover.py
+++ test/test_discover.py
@@ -42,8 +42,8 @@ class TestTasks(unittest.TestCase):
     def test_organizes_tasks_by_types(self):
         tasks = discover.tasks()
         self.assertSetEqual(set(tasks.keys()), {"analysis", "modelling"})
-        self.assertSetEqual(tasks["analysis"], {"divik", "gasvm"})
-        self.assertSetEqual(tasks["modelling"], {"gmm"})
+        self.assertSetEqual(set(tasks["analysis"]), {"divik", "gasvm"})
+        self.assertSetEqual(set(tasks["modelling"]), {"gmm"})
 
 
 @patch('celery.task.control.inspect', new=INSPECT_MOCK)
@@ -53,5 +53,90 @@ class TestRole(unittest.TestCase):
         self.assertEqual(role, "analysis")
 
     def test_throws_for_nonexistent_task(self):
-        with self.assertRaises(KeyError):
+        with self.assertRaises(ValueError):
             discover.role("blah")
+
+
+@patch('celery.task.control.inspect', new=INSPECT_MOCK)
+class TestBackend(unittest.TestCase):
+    def test_finds_the_execution_backend(self):
+        backend = discover.backend("divik")
+        self.assertEqual(backend, "divik-worker")
+
+    def test_finds_any_of_workers_that_support_algorithm(self):
+        backend = discover.backend("gmm")
+        self.assertIn(backend, {"gmm-worker", "multitask-worker"})
+
+    def test_throws_for_nonexistent_task(self):
+        with self.assertRaises(ValueError):
+            discover.backend("blah")
+
+
+SAMPLE_FINISHED_ANALYSES = {
+    "divik-worker/results/divik": [
+        {
+            "id": 123,
+            "name": "Some DiviK",
+        },
+    ],
+    "gmm-worker/results/gmm": [
+        {
+            "id": 456,
+            "name": "Some GMM",
+        },
+        {
+            "id": 789,
+            "name": "Another GMM",
+        },
+    ],
+    "multitask-worker/results/gmm": [
+        {
+            "id": 456,
+            "name": "Some GMM",
+        },
+        {
+            "id": 789,
+            "name": "Another GMM",
+        },
+    ],
+    "multitask-worker/results/gasvm": [
+    ],
+}
+
+
+def get_request_mock(json, side_effect: bool=False):
+    mock = MagicMock()
+    if not side_effect:
+        mock.__call__().json.return_value = json
+    else:
+        def side_effect(url):
+            another_mock = MagicMock()
+            another_mock.json.return_value = json[url]
+            return another_mock
+        mock.side_effect = side_effect
+    return mock
+
+
+@patch('requests.get', new=get_request_mock(SAMPLE_FINISHED_ANALYSES, True))
+class TestFinishedAnalyses(unittest.TestCase):
+    def setUp(self):
+        with patch('celery.task.control.inspect', new=INSPECT_MOCK):
+            self.workers = discover.workers()
+
+    def test_groups_analyses_by_task_name(self):
+        analyses = discover.finished_analyses(self.workers)
+        self.assertIn("divik", analyses)
+        self.assertIn("gmm", analyses)
+        self.assertIn("gasvm", analyses)
+
+    def test_proxies_all_tasks(self):
+        analyses = discover.finished_analyses(self.workers)
+        self.assertEqual(1, len(analyses["divik"]))
+        self.assertEqual(2, len(analyses["gmm"]))
+        self.assertEqual(0, len(analyses["gasvm"]))
+
+    def test_proxies_task_details(self):
+        analyses = discover.finished_analyses(self.workers)
+        divik = analyses["divik"][0]
+        self.assertEqual(divik["id"], 123)
+        self.assertEqual(divik["name"], "Some DiviK")
